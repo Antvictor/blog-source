cd4bbdb90a3d7174a5b6c52331d0bb044112aa6f
Antvictor <larryyangc@163.com>
2023-07-28 18:34:09 +0800

    feat: create ship

--- python/alien_invasion/alien_invasion.py
+++ python/alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from setting import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
@@ -10,9 +11,10 @@ class AlienInvasion:
         self.settings = Settings()
         # 设置一个像素(1200,800)的屏幕
         self.screen = pygame.display.set_mode((self.settings.screen_width, 
-            self.settings.screen_higth))
+        self.settings.screen_higth))    
         # 设置标题
         pygame.display.set_caption("Alient Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         '''开始游戏主循环'''
@@ -25,6 +27,8 @@ class AlienInvasion:
                     sys.exit()
                 # 每次循环都重绘屏幕
                 self.screen.fill(self.settings.bg_color)
+                # 加载飞船
+                self.ship.blitme()
                 # 让最近绘制的屏幕可见
                 pygame.display.flip()
 
--- python/alien_invasion/setting.py
+++ python/alien_invasion/setting.py
@@ -1,5 +1,5 @@
 class Settings:
     def __init__(self):
-        self.bg_color = (250,250,250)
+        self.bg_color = (230,230,230)
         self.screen_width = 1200
         self.screen_higth = 800
