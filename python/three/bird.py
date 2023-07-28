65287cd45753058da1719a76f7a66629778c91b2
Antvictor <larryyangc@163.com>
2023-07-28 11:38:29 +0800

    feat: rewrite

--- python/three/bird.py
+++ python/three/bird.py
@@ -4,3 +4,14 @@ class Bird:
           self.feather = feather
       def fly(self):
           print(f"this bird use {self.wings} fly and it has {self.feather}")
+
+class Penguin(Bird) :
+
+  def run(self):
+    print(f"yes, I`m run")
+
+  def fly(self):   
+    print(f"I`m Penguin, I`m can`t fly, I just glide")
+    
+  def fly(self, cry):
+    print(f"I`m Penguin, {cry}, I`m gliding")
--- python/three/main.py
+++ python/three/main.py
@@ -1,4 +1,4 @@
-from bird import Bird
+from bird import Bird, Penguin
 
 b = Bird("big wings", "green feather");
 b.fly()
@@ -7,3 +7,8 @@ del b.wings
 # b.fly() 删除后不能再用
 b.wings = "small wings"
 b.fly() #重新赋值后才能使用
+
+p = Penguin("small wings","villus");
+p.fly()
+p.run()
+p.fly("嘎嘎")
