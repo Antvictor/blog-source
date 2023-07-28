c306978e1711a2c63b24eb0607f5b078d3f1bb7d
Antvictor <larryyangc@163.com>
2023-07-28 17:50:09 +0800

    feat: Split the Setting into the Setting class

--- python/alien_invasion/alien_invasion.py
+++ python/alien_invasion/alien_invasion.py
@@ -1,16 +1,18 @@
 import sys
 import pygame
+from setting import Settings
 
 class AlienInvasion:
     def __init__(self):
         '''初始化游戏进程'''
         pygame.init()
+        # 加载设置类
+        self.settings = Settings()
         # 设置一个像素(1200,800)的屏幕
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, 
+            self.settings.screen_higth))
         # 设置标题
         pygame.display.set_caption("Alient Invasion")
-        # 设置背景颜色
-        self.bg_color = (230,230,230)
 
     def run_game(self):
         '''开始游戏主循环'''
@@ -22,7 +24,7 @@ class AlienInvasion:
                     # 关闭程序
                     sys.exit()
                 # 每次循环都重绘屏幕
-                self.screen.fill(self.bg_color)
+                self.screen.fill(self.settings.bg_color)
                 # 让最近绘制的屏幕可见
                 pygame.display.flip()
 
