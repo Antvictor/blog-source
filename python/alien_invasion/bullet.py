1cb8dde9339e63083ec9164c716cd050a444506e
Antvictor <larryyangc@163.com>
2023-07-29 10:13:42 +0800

    feat: init bullet

---
+++ python/alien_invasion/bullet.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.sprite import Sprite
+class Bullet(Sprite):
+
+    def __init__(self, ai_game):
+        super.__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
+        self.color = self.settings.bullet_color
+        # 绘制一个矩形
+        self.rect = pygame.Rect(0,0,self.settings.bullet_width, 
+            self.settings.bullet_height)
+        # 和飞船的头部在头一位置
+        self.rect.midtop = self.ship.rect.midtop
+        # 子弹的位置
+        self.y = float(self.rect.y)
+
+    def update(self):
+        '''向上移动子弹'''
+        self.y -= self.settings.bullet_speed
+        sefl.rect.y = self.y
+
+    def draw(self):
+        '''在屏幕上绘制子弹'''
+        pygame.draw(self.screen, self.color, self.rect)
--- python/alien_invasion/setting.py
+++ python/alien_invasion/setting.py
@@ -5,3 +5,8 @@ class Settings:
         self.screen_height = 800
         # 飞船移动速度
         self.ship_speed = 1.5
+        # 子弹设置
+        self.bullet_color=(60,60,60)
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
